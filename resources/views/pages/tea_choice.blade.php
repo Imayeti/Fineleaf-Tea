a5b086664fe6f4684c8ee6cd98dfcb38038ffd16
Micah Jones <maj407@gmail.com>
2018-10-29 13:50:13 -0400

    adding price and cart button to tea_choice

--- resources/views/pages/tea_choice.blade.php
+++ resources/views/pages/tea_choice.blade.php
@@ -1,22 +1,36 @@
 @extends('layout')
 
 @section('title')
+
     Black Tea
 @endsection
 
 @section('content')
 
 
-<h1 class="text-center mt-5 mb-5">Black Tea</h1>
+<h1 class="text-center mt-5 mb-5 border-top border-bottom border-light">Black Tea</h1>
 
 <div class="row container " style=" margin: 0 auto;">
 
-      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 mb-3">
+        <div class="card card-center border-none">
+          <img class="card-img-top" src="images/Golden-Monkey-Black.jpg" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text text-center">golden monkey black tea</p>
+          </div>
+        </div>
+        <div class="mb-3 text-center">
+        1 oz. - $15.00
+      </div class="">
+        <button type="button" class="btn btn-light center-it">Add To Cart</button>
 
+      </div>
+
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
         <div class="card card-center">
           <img class="card-img-top" src="images/Golden-Monkey-Black.jpg" alt="Card image cap">
           <div class="card-body">
-            <p class="card-text">Our award winning golden monkey black tea</p>
+            <p class="card-text">golden monkey black tea</p>
           </div>
         </div>
       </div>
@@ -25,7 +39,7 @@
         <div class="card card-center">
           <img class="card-img-top" src="images/Golden-Monkey-Black.jpg" alt="Card image cap">
           <div class="card-body">
-            <p class="card-text">Our award winning golden monkey black tea</p>
+            <p class="card-text">golden monkey black tea</p>
           </div>
         </div>
       </div>
@@ -34,16 +48,7 @@
         <div class="card card-center">
           <img class="card-img-top" src="images/Golden-Monkey-Black.jpg" alt="Card image cap">
           <div class="card-body">
-            <p class="card-text">Our award winning golden monkey black tea</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-        <div class="card card-center">
-          <img class="card-img-top" src="images/Golden-Monkey-Black.jpg" alt="Card image cap">
-          <div class="card-body">
-            <p class="card-text">Our award winning golden monkey black tea</p>
+            <p class="card-text">golden monkey black tea</p>
           </div>
         </div>
       </div>
@@ -59,7 +64,7 @@
         <div class="card card-center">
           <img class="card-img-top" src="images/Golden-Monkey-Black.jpg" alt="Card image cap">
           <div class="card-body">
-            <p class="card-text">Our award winning golden monkey black tea</p>
+            <p class="card-text">golden monkey black tea</p>
           </div>
         </div>
       </div>
@@ -68,7 +73,7 @@
         <div class="card card-center">
           <img class="card-img-top" src="images/Golden-Monkey-Black.jpg" alt="Card image cap">
           <div class="card-body">
-            <p class="card-text">Our award winning golden monkey black tea</p>
+            <p class="card-text">golden monkey black tea</p>
           </div>
         </div>
       </div>
@@ -77,7 +82,7 @@
         <div class="card card-center">
           <img class="card-img-top" src="images/Golden-Monkey-Black.jpg" alt="Card image cap">
           <div class="card-body">
-            <p class="card-text">Our award winning golden monkey black tea</p>
+            <p class="card-text">golden monkey black tea</p>
           </div>
         </div>
       </div>
@@ -86,7 +91,7 @@
         <div class="card card-center">
           <img class="card-img-top" src="images/Golden-Monkey-Black.jpg" alt="Card image cap">
           <div class="card-body">
-            <p class="card-text">Our award winning golden monkey black tea</p>
+            <p class="card-text">golden monkey black tea</p>
           </div>
         </div>
       </div>
