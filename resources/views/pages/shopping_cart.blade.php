e480254e1a86aea1c70a0dd63f0e3a857d1be221
Micah Jones <maj407@gmail.com>
2018-11-26 21:22:55 -0500

    fixing cart display and for mobile

--- resources/views/pages/shopping_cart.blade.php
+++ resources/views/pages/shopping_cart.blade.php
@@ -12,30 +12,37 @@
 <div class="">
 
 @if($userProducts)
-  <table  id="t01"  class="table">
+  <table  id="t01"  class="table display-none">
     <tbody>
     <tr class="border-bottom">
       <th class="pl-5">PRODUCTS</th>
       <th>Qt.</th>
+
       <th>PRICE</th>
       <th>TOTAL</th>
+      <th></th>
 @foreach ($temp as $product)
     </tr>
     <tr class="border-bottom">
-      <td ><image class="ml-4 mr-5 zero-margins"src="{{ $product->img_src }}" style="max-width:75px;"><strong>{{ $product->name }}</strong>&nbsp -<span class="ml-1">{{ $product->short_description }}</td>
-      <td ><form action="/shopping_cart/{{ $product->id }}/edit" method="get">
-
-              <input type="number" name="requestQuantity" min="1" max="50" value="{{ $product->quantity }}">
-              <button type="submit" class="btn btn-light ml-5 mr-4 btn-sm no-margin">apply</button>
-          </form>
+      <td >
+        <image class="ml-4 mr-5 zero-margins"src="{{ $product->img_src }}" style="max-width:75px;">
+        <strong>{{ $product->name }}</strong>&nbsp -<span class="ml-1">{{ $product->short_description }}
+      </td>
+      <td>
+        <form action="/shopping_cart/{{ $product->id }}/edit" method="get">
+            <input type="number" name="requestQuantity" min="1" max="50" value="{{ $product->quantity }}">
+            <button type="submit" class="btn btn-light ml-5 mr-4 btn-sm no-margin">apply</button>
+        </form>
       </td>
       <td>${{ $product->price }}.00</td>
       <td>${{ $product->price * $product->quantity }}.00
-              <form class="" action="/shopping_cart/{{ $product->id }}" method="post">
-                {{ method_field('DELETE') }}
-                {{ csrf_field() }}
-                <button type="submit" class="button btn btn-primary">DELETE</button>
-              </form>
+      </td>
+      <td>
+        <form class="" action="/shopping_cart/{{ $product->id }}" method="post">
+            {{ method_field('DELETE') }}
+            {{ csrf_field() }}
+            <button type="submit" class="button btn-sm btn btn-light btn-primary">DELETE</button>
+        </form>
       </td>
     </tr>
 @endforeach
@@ -50,11 +57,13 @@
     <tr>
       <th class="border-top-0"></th>
       <th class="border-top-0"></th>
+      <th class="border-top-0"></th>
       <th class="border-top-0">CART TOTALS</th>
       <th class="border-top-0">&nbsp</th>
 
     </tr>
     <tr class="border-0">
+      <th class="border-top-0"></th>
       <th class="border-top-0"></th>
 
       <td class="border-top-0" >&nbsp</td>
@@ -64,6 +73,7 @@
     </tr>
     <tr>
       <th class="border-top-0"></th>
+      <th class="border-top-0"></th>
 
       <td class="border-top-0">&nbsp</td>
       <td>SHIPPING</td>
@@ -71,6 +81,7 @@
 
     </tr>
     <tr>
+      <th class="border-top-0"></th>
       <th class="border-top-0"></th>
 
       <td class="border-top-0">&nbsp</td>
@@ -81,6 +92,74 @@
   </tfoot>
 
   </table>
+
+
+
+
+  @foreach ($temp as $product)
+      <div class="container-fluid row pb-2 display-on-small display-off-large">
+
+
+        <div class="col-md-4 col-xs-8"><image class="ml-4 mr-5 zero-margins"src="{{ $product->img_src }}" style="max-width:75px;"><strong>{{ $product->name }}</strong>&nbsp -<span class="ml-1">{{ $product->short_description }}
+        </div>
+
+
+        <!-- <div class="col-md-2 col-xs-4"><form action="/shopping_cart/{{ $product->id }}/edit" method="get">
+
+                <input type="number" name="requestQuantity" min="1" max="50" value="{{ $product->quantity }}">
+                <button type="submit" class="btn btn-light ml-5 mr-4 btn-sm no-margin">apply</button>
+            </form>
+        </div>
+        <div class="col-md-2 col-xs-4">${{ $product->price }}.00</div>
+        <div class="col-md-2 col-xs-4">${{ $product->price * $product->quantity }}.00</div>
+        <div class="col-md-2 col-xs-4">
+                <form class="" action="/shopping_cart/{{ $product->id }}" method="post">
+                  {{ method_field('DELETE') }}
+                  {{ csrf_field() }}
+                  <button type="submit" class="button btn-sm btn btn-light btn-primary">DELETE</button>
+                </form>
+        </div> -->
+      </div>
+
+
+
+  @endforeach
+  <div class="clearfix">
+
+  <table class="border table mt-4 pb-4 mb-4 float-right">
+        <tr>
+
+  <th class="border-top-0">CART TOTALS</th>
+  <th class="border-top-0">&nbsp</th>
+
+  </tr>
+  <tr class="">
+
+  <td>SUBTOTAL</td>
+  <td>${{$subtotal}}.00 </td>
+
+  </tr>
+  <tr>
+
+  <td>SHIPPING</td>
+  <td>$10.00</td>
+
+  </tr>
+  <tr>
+
+  <td>TOTAL</td>
+  <td>${{$total}}.00</td>
+
+  </tr>
+  </table>
+
+</div>
+
+
+
+
+
+
 
 <div class="clearfix">
 
@@ -97,4 +176,17 @@
   </div>
 
 @endif
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
